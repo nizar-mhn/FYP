2c37f3153e5599abd6dd110415d280b7198a27af
Nyzr <nizarhamid80@gmail.com>
2022-12-02 11:54:21 +0800

    Drive picker view created

--- resources/views/students/main.blade.php
+++ resources/views/students/main.blade.php
@@ -23,13 +23,14 @@
                     <div class="me-auto subjectButtons">
                         <button type="button" class="btn text-light text-wrap ">A2BS-something</button>
                         <button type="button" class="btn text-light text-wrap ">A3BS-something</button>
+                        <a href="" id="result"></a>
                     </div>
                     <div class="ms-auto dropdown">
                         <button type="button" class="btn uploadButton text-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Upload</button>
                         <ul class="dropdown-menu">
                             <li><button type="button" class="btn text-wrap dropdown-item">File Upload</button></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><button type="button" class="btn text-wrap dropdown-item" onclick="pickerDialog()">Google Drive Upload</button></li>
+                            <li><button type="button" class="btn text-wrap dropdown-item" onclick="createPicker()">Google Drive Upload</button></li>
                         </ul>
                     </div>
                 </div>
@@ -43,70 +44,84 @@
         </div>
     </div>
 
+    /* var clientID = '984571528719-5klivvs955pte6vctv52us32pmtd4t90.apps.googleusercontent.com'
+    var apiKey = 'AIzaSyCy_O5QlJzeQ-AjlDrOmkhAy9idRwDctx0'
+    var projectID = '984571528719'
+    var oauthtoken
+    var pickerApiLoaded = false
+
+    //scope for google drive
+    var scope = 'https://www.googleapis.com/auth/drive.file'; */
 @endsection
 @push('scripts')
     <script>
-        var clientID = '984571528719-5klivvs955pte6vctv52us32pmtd4t90.apps.googleusercontent.com'
-        var apiKey = 'AIzaSyCy_O5QlJzeQ-AjlDrOmkhAy9idRwDctx0'
-        var projectID = '984571528719'
-        var oauthtoken
-        var pickerApiLoaded = false
-    
-        //scope for google drive
-        var scope = 'https://www.googleapis.com/auth/drive.file';
-    
-        //client library google
-        function pickerDialog(){
-            loadPicker()
-        }
-    
-        function loadPicker(){
-            gapi.load('auth',{'callback':onAuthApiLoad})
-            gapi.load('picker',{'callback':onPickerApiLoad})
-        }
-    
-        function onAuthApiLoad(){
-            window.gapi.auth.authorize({
-                'client_id': clientID,
-                'scope':scope,
-                'immediate':false
-            },
-            handleAuthResult
-            )
-        }
-    
-        function handleAuthResult(authResult){
-            //Access token
-            if(authResult && !authResult.error){
-                oauthToken = authResult.access_token
-                createPicker()
-            }
-        }
-    
-        function onPickerApiLoad(){
-            pickerApiLoaded = true
-            createPicker()
-        }
-    
-        function createPicker(){
-            if(pickerApiLoaded && oauthtoken){
-                var view = new google.picker.View(google.picker.ViewId.DOCS)
-                view.setMimeTypes('image/png,image/jpeg,image/jpg');
-                var picker = new google.picker.PickerBuilder()
-                .enableFeature(google.picker.Feature.NAV_HIDDEN)
-                .setAppId(project_id)
-                .setOAuthToken(oauthtoken)
-                .addView(view)
-                .addView(new google.picker.DocsUploadView())
-                .setDeveloperKey(apiKey)
-                .setCallback(pickerCallback)
-                .build()
-                
-                picker.setVisible(true)
-    
-            }
-        }
-        
-    </script>
+        let tokenClient;
+        let accessToken = null;
+        let pickerInited = false;
+        let gisInited = false;
 
+        function onApiLoad() {
+            gapi.load('picker', onPickerApiLoad);
+        }
+
+        function onPickerApiLoad() {
+            pickerInited = true;
+        }
+
+        function gisLoaded() {
+            // TODO(developer): Replace with your client ID and required scopes
+            tokenClient = google.accounts.oauth2.initTokenClient({
+                client_id: '984571528719-5klivvs955pte6vctv52us32pmtd4t90.apps.googleusercontent.com',
+                scope: 'https://www.googleapis.com/auth/drive.file',
+                callback: '', // defined later
+            });
+            gisInited = true;
+        }
+
+        function createPicker() {
+            const showPicker = () => {
+                const view = new google.picker.View(google.picker.ViewId.DOCS);
+                // TODO(developer): Replace with your API key
+                const picker = new google.picker.PickerBuilder()
+                    .enableFeature(google.picker.Feature.NAV_HIDDEN)
+                    .addView(view)
+                    .setOAuthToken(accessToken)
+                    .setDeveloperKey('AIzaSyCy_O5QlJzeQ-AjlDrOmkhAy9idRwDctx0')
+                    .setCallback(pickerCallback)
+                    .build();
+                picker.setVisible(true);
+            }
+
+            // Use Google Identity Services to request an access token
+            tokenClient.callback = async (response) => {
+                if (response.error !== undefined) {
+                throw (response);
+                }
+                accessToken = response.access_token;
+                showPicker();
+            };
+
+            if (accessToken === null) {
+                    // Prompt the user to select a Google Account and ask for consent to share their data
+                    // when establishing a new session.
+                    tokenClient.requestAccessToken({prompt: 'consent'});
+                } else {
+                    // Skip display of account chooser and consent dialog for an existing session.
+                    tokenClient.requestAccessToken({prompt: ''});
+                }
+        }
+
+        // A simple callback implementation.
+        function pickerCallback(data) {
+            let url = 'nothing';
+            if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+                let doc = data[google.picker.Response.DOCUMENTS][0];
+                url = doc[google.picker.Document.ID];
+            }
+            let message = `You picked: ${url}`;
+            document.getElementById('result').innerText = message;
+        }
+    </script>
+    <script async defer src="https://apis.google.com/js/api.js" onload="onApiLoad()"></script>
+    <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
 @endpush
