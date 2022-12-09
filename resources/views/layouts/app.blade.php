9b5214de1922a74793072eb236e9a76930c91396
okito123 <chanowen123@gmail.com>
2022-12-10 01:03:32 +0800

    logo and name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
+    <link rel="icon" href="{{asset('image/logoPrintingTransparent.png')}}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,31 +17,30 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    
+
     <!-- Style -->
     <style>
-       .navbar{
-        background-color: #264653;
-       }
-
-       ul.navbar-nav a:hover{
-        border-radius: 10px;
-        background-color: #2A9D8F !important
-       }
-
-       ul.navbar-nav button:hover{
-        border-radius: 10px;
-        background-color: #2A9D8F !important
-       }
-
-       .dropdown-toggle::after {
-         display:none;
+        .navbar {
+            background-color: #264653;
         }
-       
-       
+
+        ul.navbar-nav a:hover {
+            border-radius: 10px;
+            background-color: #2A9D8F !important
+        }
+
+        ul.navbar-nav button:hover {
+            border-radius: 10px;
+            background-color: #2A9D8F !important
+        }
+
+        .dropdown-toggle::after {
+            display: none;
+        }
     </style>
 
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark shadow-sm navbarColor">
@@ -47,7 +48,7 @@
 
                 <a class="navbar-brand" href="{{ route('document') }}">
                     <img src="../image/logoPrintingTransparent.png" alt="School Logo" width="30" height="30">
-                    Student Printing 
+                    TAR UMT Printing
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -56,7 +57,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -80,43 +81,41 @@
                             <a class="nav-link" href="{{ route('studentHistory') }}">History</a>
                         </li>
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ url('../') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('../') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->studentName }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->studentName }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile') }}" onclick="event.preventDefault();
+                                                     document.getElementById('profile-form').submit();">
+                                    {{ __('Profile') }}
+                                </a>
+                                <form id="profile-form" action="{{ route('profile') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('profile') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('profile-form').submit();">
-                                        {{ __('Profile') }}
-                                    </a>
-                                    <form id="profile-form" action="{{ route('profile') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -124,11 +123,12 @@
         </nav>
         <div class="container-fluid" style="background-color:#2A9D8F; height:25px;">
         </div>
-        
+
         <main class="py-4">
             @yield('content')
         </main>
     </div>
     @stack('scripts')
 </body>
+
 </html>
