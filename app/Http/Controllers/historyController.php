0af0694e3fe095e1862374a03a47fe01c43a6520
Nyzr <nizarhamid80@gmail.com>
2022-12-09 23:57:22 +0800

    History page

--- app/Http/Controllers/historyController.php
+++ app/Http/Controllers/historyController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class historyController extends Controller
 {
     public function index()
     {
-        return view('students/history');
+        $currentUserID = Auth::user()->studentID;
+        $currentUserOrderPrintingInfoID = DB::table('orders')
+        ->join('order_details','orders.orderID', "=", 'order_details.orderID')
+        ->where('orders.studentID',$currentUserID)->orderBy('orderDate','desc')->get();
+        
+        return view('students/history',[
+            'printingInfoID' => $currentUserOrderPrintingInfoID,
+        ]);
     }
 }
--- resources/views/students/history.blade.php
+++ resources/views/students/history.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@php
+    use App\Models\payment;
+@endphp
 <style>
     .orderDetails:hover{
         background-color: #264653 !important;
@@ -18,19 +21,51 @@
         <div class="col"></div>
         <div class="col-8 p-2">
             <h1 class="fw-bold mb-5">History</h1>
-            <div class="d-flex p-1 mb-4" style="background-color: #264653; height: 45px">
-                <div class="me-auto filterButtons">
-                    <button type="button" class="btn text-light text-wrap ">Date <i class="bi bi-sort-up"></i></button>
-                    <button type="button" class="btn text-light text-wrap ">Price <i class="bi bi-sort-up"></i></button>
+            <div class="row align-items-center " style="background-color: #264653; height: 45px">
+                <div class="col-md-2 text-light text-center">
+                    Image  
                 </div>
-                <div class="ms-auto">
-                    <button type="button" class="btn searchButton text-light" style="background-color: #E76F51"><i class="bi bi-search" style=""></i>  Search</button>
-                    
+                <div class="col-md-3 text-light text-center">
+                    Order Info
+                </div>
+                <div class="col-md-2 offset-md-3 text-light">
+                    Price
+                </div>
+                <div class="col-md-2 text-light">
+                    Status
                 </div>
             </div>
-    
-            <div class="container-fluid mt-3 orderDetails" style="background-color: #2A9D8F; height: 70px; border-radius:22px"></div>
-            <div class="container-fluid mt-3 orderDetails" style="background-color: #2A9D8F; height: 70px; border-radius:22px"></div>
+            @foreach ($printingInfoID as $id)
+            @php
+                
+                $orderInfo = DB::table('order_printing_infos')->where('orderPrintingInfoID',$id->orderPrintingInfoID)->first();
+                $file = DB::table('files')->where('fileID',$orderInfo->fileID)->first();
+                $payment = Payment::where('orderID',$id->orderID)->first();
+            @endphp
+            <div class="row mt-3 align-items-center orderDetails" style="background-color: #2A9D8F; border-radius:22px">
+                <div class="col-md-2">
+                    <img src="data:image/png;base64,{{ $file->thumbnail }}" alt="" class="img-fluid p-1 thumbnail border border-2">
+                </div>
+                <div class="col-md-3 text-light">
+                    <h4>{{ $file->fileName }}</h4>
+                    <p>Binding Type: {{ str_replace('_',' ',$orderInfo->bindingType) }}</p>
+                    <p>Color: {{ $orderInfo->color }}</p>
+                    @if ($orderInfo->pageFormat == 'one-side')
+                    <p>Page Format: One-Sided Page</p>
+                    @else
+                    <p>Page Format: Back-to-Back Page</p>
+                    @endif
+                    <p>Number of Copies: {{ $orderInfo->numCopies }}</p>
+                </div>
+                <div class="col-md-2 offset-md-3 text-light">
+                    <h4>Price: </h4>
+                    <h4>RM {{number_format((float)$payment->totalPrice, 2, '.', '')}}</h4>
+                </div>
+                <div class="col-md-2 text-light">
+                    {{ $id->status }}
+                </div>
+            </div>
+            @endforeach 
         </div>
         <div class="col"></div>
     </div>
